22182ad38dd63fc19d18abc8855fde1c0fb15d6f
Veselin Dechev <veskopos@mail.bg>
2016-03-13 02:21:08 +0200

    #4 Done with Contact page and Change CSS
    Adding Google map and address at Contact page and making it relative.

---
+++ WebContent/scripts/contact.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	"use strict";
+	
+	$(".jumbotron h1").text("Driving License");
+	
+	$("#login a").text("Login");
+	
+	$("#register a").text("Register");
+	
+	$(".jumbotron #1").text("Можете да ни намерите на адрес:");
+	
+	$(".jumbotron #2").text("Холивуд Лос Анджелис, Калифорния Съединени щати");
+});
